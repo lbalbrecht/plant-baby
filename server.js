ac6b939d46751ad514f200d34735fe9e9067262f
lbalbrecht <lalbrecht18@wooster.edu>
2021-06-06 18:47:45 -0700

    taking out slashes

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //LOCAL
 
 //deployed
-app.use(cors({origin:["https://plantbaby.herokuapp.com/"]}));
+app.use(cors({origin:["https://plantbaby.herokuapp.com"]}));
 
 
 app.use(routes);
